579c78835c38e1cfde399696b6aa6f2696d3b291
Rav53 <Rav5385@gmail.com>
2022-12-23 18:47:01 +0300

    дз4

--- HomeWork4/Zadacha2.java
+++ HomeWork4/Zadacha2.java
@@ -1,39 +1,31 @@
+//Определить является ли список знакочередующимся? (без массивов)
 import java.util.LinkedList;
+import java.util.Scanner;
+
 
 public class Zadacha2 {
     public static void main(String[] args) {
-        LinkedList<Integer> taskList = new LinkedList<>();
-        taskList.add(-1);
-        taskList.add(32);
-        taskList.add(-5);
-        taskList.add(6);
-        taskList.add(7);
-        System.out.println("Изначальный список:");
-        System.out.println(taskList.toString());
-        // System.out.println("Список знакочередующийся");
-        // System.out.println(taskList.toString());
+
+    LinkedList<Integer> integerLinkedList = new LinkedList<>();
+        try (Scanner scanner = new Scanner(System.in)) {
+            int numA;
+            int numB;
+            boolean condition = true;   
+            System.out.println("введите размер списка:");
+            int number = scanner.nextInt();
+            System.out.println("введите значения каждого элемента списка:");
+            numA = scanner.nextInt();
+            integerLinkedList.add(numA);
+            for (int i = 1; i <= number - 1; i++) {
+            numB = scanner.nextInt();
+            integerLinkedList.add(numB);
+                if (((numA > 0) && ((numB * (-1)) < 0)) || ((numA < 0) && ((numB * (-1)) > 0))) {
+                    condition = false;
+                }
+                numA = numB;
+            }
+            System.out.print("список знакочередующийся - " + condition);
+        }
+        
     }
 }
-//        boolean flag = true;
-//        if (taskList.Count > 0);{
-//
-//        boolean minus = true;
-//            for (int i = 1; i < 0; i++);
-//                if (minus && taskList[i] < 0);{
-//                    flag = false;
-//                    break;
-//                }
-//                if (!minus && taskList[i] > 0);{
-//                    flag = false;
-//                    break;
-//                }
-//        }
-//             if flag == True;{
-//        System.out.println("Список знакочередующийся");
-//        }
-//             else System.out.println("Последовательность не знакочередующаяся.");
-//}
-//
-         
-    
-
